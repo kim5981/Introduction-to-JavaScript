7f6869ca95c5ff4e03dacec4f15e9474b85c425c
kim5981 <kimberlyruthrodriguez@gmail.com>
2022-01-11 17:38:59 -0800

    completed task 3

--- index.js
+++ index.js
@@ -136,20 +136,28 @@ NOTE: If done correctly, a weight of 15 lbs and age of 1 year would return 0.449
 */  
 
 function hungryDog(weight, age){
-  if (weight >= 5 && age >= 1) {
+  if (weight <= 5 && age >= 1) {
     return (weight * .05);
-  } else if (weight >=6 && age >= 1) {
+  } else if (weight >=6 && age >= 1 && weight <= 10) {
     return (weight * .04);
-   } else if (weight >= 11 && age >= 1) {
+   } else if (weight >= 11 && age >= 1 && weight <= 15) {
      return (weight * .03);
    } else if (weight > 15 && age >= 1) {
      return (weight * .02);
+     //puppy weight vvvv
+   } else if (age <1 && age >= .583 ) {
+     return (weight * .04);
+   }  else if (age < .583 && age >= .333) {
+     return (weight * .05);
+   } else if ( age < .333) {
+     return (weight * .1);
+   } else {
+     return "remeasure the food :("
    }
     
-
-  
-
 }
+
+
 
 
 
